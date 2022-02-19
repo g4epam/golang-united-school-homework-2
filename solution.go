fdab49dbfc0f1f14be485304be6b86c104a86400
g4epam <gtsitskishvili4epam@gmail.com>
2022-02-19 10:12:57 +0400

    first time by mistake pushed with another account

--- solution.go
+++ solution.go
@@ -3,7 +3,6 @@ package square
 import "math"
 
 // Define custom int type to hold sides number and update CalcSquare signature by replacing #yourTypeNameHere#
-
 // Define constants to represent 0, 3 and 4 sides.  Test uses mnemos: SidesTriangle(==3), SidesSquare(==4), SidesCircle(==0)
 // it's like:
 // CalcSquare(10.0, SidesTriangle)
